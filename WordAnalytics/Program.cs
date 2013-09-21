854c429a196d87eaada5d52377e51f146e73d09c
Glenruben <glenruben@gmail.com>
2013-09-21 22:21:55 +0200

    Initial commit

---
+++ WordAnalytics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WordAnalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\Downloads\\lorem_ipsum.txt";
+
+            
+            
+            System.Console.WriteLine("Hello whatever");
+
+            System.Console.Read();
+
+        }
+    }
+}
