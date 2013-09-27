fc837ad0121fbbbbe6fbd5e98a3253e28b4ad653
Glenruben <glenruben@gmail.com>
2013-09-27 02:32:54 +0200

    Started separate class for counting repeated occurrence of word and returning top three most used words in a string

---
+++ WordAnalytics/WordUse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordAnalytics
+{
+    class WordUse
+    {
+        public Array mostUsed( string s )
+        {
+            
+            MatchCollection collection = Regex.Matches( s.ToLower(), @"[\S]+"); //collection of words in text, case ignored. TODO: Find unique words and count instances!
+
+            
+
+        }
+    }
+}
