f702f89715abd64544e59d78a8fa21222440961d
Glenruben <glenruben@gmail.com>
2013-10-06 16:39:58 +0200

    unfinished work on the counter method for most-used words

--- WordAnalytics/WordUse.cs
+++ WordAnalytics/WordUse.cs
@@ -14,7 +14,21 @@ namespace WordAnalytics
             
             MatchCollection collection = Regex.Matches( s.ToLower(), @"[\S]+"); //collection of words in text, case ignored. TODO: Find unique words and count instances!
 
+            List<string> collectionList = new List<string>;
+
+            foreach (Match match in collection)
+            {
+                collectionList.Add(match);
+            }
+
+            string wordString = collection.ToString();
+
             
+
+            List<String> counter = wordString.GroupBy(x => x)
+                                            .Where(g => g.Count() > 1)
+                                            .Select(g => g.Key)
+                                            .ToList();
 
         }
     }
